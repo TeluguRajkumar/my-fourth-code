eb1c52874616363b75811e246a353e584d378626
Rajkumar Telugu <71225421+TeluguRajkumar@users.noreply.github.com>
2020-11-24 12:10:57 +0530

    Add files via upload
    This is used to get any website IP address using python

---
+++ get website IP address.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 24 11:47:12 2020
+
+@author: rajkumar
+"""
+
+import socket as s
+host = 'copyassignment.com'
+print(f'IP of {host}is{s.hethostbyname(host)}')
